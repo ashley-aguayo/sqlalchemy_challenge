c306975b8e2bae41c4e510f1f2743283045611d4
ashley-aguayo <ashley.aguayo@outlook.com>
2021-05-22 17:01:28 -0700

    update

--- app.py
+++ app.py
@@ -1,1 +1,44 @@
-from sqlalchemy import create_engine
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+
+# reflect the tables
+Base.prepare(engine, reflect=True)
+print(Base.classes.keys())
+
+# Save reference to the table
+measurement = Base.classes.measurement
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+
+# Flask Routes
+#################################################
+
+@app.route("/")
+def home():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/preciptation<br/>"
+        f"/api/v1.0/Stations"
+    )
+
+if _name__ == "__main__":
+    app.run(debug=True)
